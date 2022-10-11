24e1c64069bbe1fa61654ad16caa6460277ab479
Tzvi H <16guitar@gmail.com>
2022-10-11 16:12:36 -0600

    Initial commit

---
+++ src/Showcase.js
@@ -0,0 +1,28 @@
+import BulbasaurImage from "./images/bulbasaur.jpg";
+
+function Showcase() {
+  const favPokemon = "Bulbasaur";
+  const pokeCharacteristics = {
+    type: "Grass",
+    move: "Vine Whip"
+  };
+
+  return (
+    <div>
+      <h1>{favPokemon}'s Showcase Component</h1>
+      <img src={BulbasaurImage} alt={favPokemon} />
+      <h2>
+        {favPokemon}'s type is{" "}
+        <span style={{ backgroundColor: "green", color: "white" }}>
+          {pokeCharacteristics.type}
+        </span>{" "}
+        and one of their moves is{" "}
+        <span style={{ backgroundColor: "white", color: "#00FF00" }}>
+          {pokeCharacteristics.move}
+        </span>
+      </h2>
+    </div>
+  );
+}
+
+export default Showcase;
